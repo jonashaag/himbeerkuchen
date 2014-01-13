47b7549d3daa2a53825a446bbd50cb9aab322aa4
Jonas Haag <jonas@lophus.org>
2014-01-13 10:36:25 +0100

    Rework wecker

---
+++ radio.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+. ./stations
+
+play_random_station
--- wecker.sh
+++ wecker.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
+. ./stations
+
 volume() {
-  amixer sset PCM playback $1%
+echo "$1"
+  amixer -M sset PCM playback $1% >/dev/null
 }
 
-# Reset volume
-volume 75
-
-# Start moc server
-mocp -S
-
-# Start playback
-mocp --playit "http://pm-srv.de:29000"
+# Give the loudspeakers a chance to wake up
+volume 30
+play_random_station()
+sleep 5
 
 # Continuously increase volume
-for vol in $(seq 76 92); do
-  sleep 120
+for vol in $(seq 0 65); do
   volume $vol
+  sleep 10
 done
+
+sleep 60
 
 # If I haven't woken up at this point, I never will.
 mocp --exit
